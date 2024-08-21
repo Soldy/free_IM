363a26236180f222d62cd07dcf85acab3923c2f9
Soldy <4786022+Soldy@users.noreply.github.com>
2024-08-21 19:19:42 +0100

    minimal teams client
    Signed-off-by: Soldy <4786022+Soldy@users.noreply.github.com>

---
+++ microsoft_teams/app/src/main/java/com/free/teams/MainActivity.java
@@ -0,0 +1,48 @@
+package com.free.teams;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.splashscreen.SplashScreen;
+import android.util.Log;
+import android.webkit.ConsoleMessage;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.RenderProcessGoneDetail;
+
+public class MainActivity extends AppCompatActivity {
+    public WebView mWebView;
+    public void initWebView(){
+        mWebView = new WebView(this);
+        WebSettings mSettings = mWebView.getSettings();
+        mSettings.setLoadWithOverviewMode(false);
+        mWebView.setPadding(0,0,0,0); 
+        mSettings.setUseWideViewPort(false);
+        mSettings.setSupportZoom(false);
+        mSettings.setBuiltInZoomControls(false);
+        mSettings.setDisplayZoomControls(false);
+        mSettings.setJavaScriptEnabled(true);
+        mSettings.setDomStorageEnabled(true);
+        mWebView.setWebChromeClient(new WebChromeClient(){}); 
+        mWebView.setWebViewClient(new WebViewClient() {
+          public boolean onRenderProcessGone(
+            WebView view,
+            RenderProcessGoneDetail detail
+          ){
+            mWebView.destroy();
+            initWebView();
+            return true;
+          }
+        });
+       
+        mWebView.loadUrl("https://teams.microsoft.com/v2/");
+        setContentView(mWebView);
+    }
+    protected void onCreate(Bundle savedInstanceState) {
+        SplashScreen splash = SplashScreen.installSplashScreen(this);
+        super.onCreate(savedInstanceState);
+        initWebView();
+    }
+}
